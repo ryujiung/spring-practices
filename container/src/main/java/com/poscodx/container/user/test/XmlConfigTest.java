19c056c0f0af9b47a63f083449d913d57b22cb13
ryujiung <ryujiung@localhost>
2023-10-04 10:52:53 +0900

    container #2

---
+++ container/src/main/java/com/poscodx/container/user/test/XmlConfigTest.java
@@ -0,0 +1,76 @@
+package com.poscodx.container.user.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.poscodx.container.user.User;
+
+public class XmlConfigTest {
+
+	public static void main(String[] args) {
+		// XML Auto Configuration(Annotaion Scanning)
+		// testApplicationContext01();
+		
+		// XML Auto Configuration(Explicit Configuration)
+		testApplicationContext02();
+		
+		// XML Auto Configuration(Annotaion Scanning)
+		// testBeanFactory01();
+		
+		// XML Auto Configuration(Explicit Configuration)
+		// testBeanFactory02();
+		
+		
+	}
+
+	
+
+	private static void testApplicationContext01() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/poscodx/container/user/applicationContext01.xml");
+		
+		User user = null;
+		user = ac.getBean(User.class);
+		System.out.println(user.getName());
+		
+		//Annotation Scan(Auto Configuration) 에서는 Bean id가 자동으로 부여된다.
+		user = (User)ac.getBean("user");
+		System.out.println(user.getName());
+	}
+	
+	private static void testApplicationContext02() {
+ApplicationContext ac = new ClassPathXmlApplicationContext("com/poscodx/container/user/applicationContext02.xml");
+		
+		User user = null;
+		
+		// Type으로 빈 가져오기
+		user = ac.getBean(User.class);
+		System.out.println(user.getName());
+		
+		//id로 빈 가져오기
+		 user = (User)ac.getBean("user");
+		 System.out.println(user.getName());
+		 
+		//name으로 빈 가져오기
+		 user = (User)ac.getBean("usr");
+		 System.out.println(user.getName());
+		
+	}
+
+	private static void testBeanFactory02() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/poscodx/container/user/applicationContext02.xml"));
+		User user = bf.getBean(User.class);
+		System.out.println(user.getName());
+		
+	}
+
+	private static void testBeanFactory01() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/poscodx/container/user/applicationContext01.xml"));
+		 User user = bf.getBean(User.class);
+		 System.out.println(user.getName());
+		
+	}
+
+}
