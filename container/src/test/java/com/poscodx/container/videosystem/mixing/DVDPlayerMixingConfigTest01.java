067cfa42f4abb12558cbfd09d77b29139cf7b82b
ryujiung <ryujiung@localhost>
2023-10-05 14:03:19 +0900

    container finish

---
+++ container/src/test/java/com/poscodx/container/videosystem/mixing/DVDPlayerMixingConfigTest01.java
@@ -0,0 +1,21 @@
+package com.poscodx.container.videosystem.mixing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.poscodx.container.videosystem.DVDPlayer;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DVDPlayerConfig.class})
+public class DVDPlayerMixingConfigTest01 {
+	@Autowired
+	DVDPlayer dvdPlayer;
+	
+	public void testPlay() {
+		assertEquals("Playing Movie MARVEL's Avengers Infinite War",dvdPlayer.play());
+	}
+}
